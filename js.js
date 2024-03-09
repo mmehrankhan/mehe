5bcf6a9ecd8c73078c0b5eb92822bf1e479a311d
mmehrankhan <154292817+mmehrankhan@users.noreply.github.com>
2024-03-09 11:00:11 +0500

    Add files via upload

---
+++ js.js
@@ -0,0 +1,27 @@
+
+// var user_pass=+prompt('Enter your password')
+// var  pass='123'
+// if(user_pass == pass){
+//     console.log('password is corret');
+// }
+
+// else{
+//     console.log('password is wrong');
+// }
+
+// var alphabate=+prompt('Enter any alphabate')
+// if(alphabate=='a'||alphabate=='e'||alphabate=='i'||alphabate=='o'||alphabate=='u'){
+//     console.log(alphabate+' is vowel');
+// }
+// else {
+//     console.log(alphabate+' is not vowel');
+// }
+
+
+// Arry
+var fruits=['mango','banana','orange','apple','pear','apricot'];
+var length=fruits.length
+
+console.log(fruits);
+console.log(length);
+console.log(fruits[2]);
